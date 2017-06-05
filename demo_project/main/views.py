8bafc39da557c3d4267e63f957e8927e71ad560f
Dan Hook <danh00k@hushmail.com>
2017-06-05 15:22:53 +0000

    If a user enters a string with an invalid base pair, the server will return the query string with the invalid base pair highlighted.

--- demo_project/main/views.py
+++ demo_project/main/views.py
@@ -16,12 +16,27 @@ def home(request):
 
     # Check whether this request includes a query string to translate.
     query_string = request.GET.get('query', None)
+    error = False
     if query_string:
         query_string = query_string.upper()
         reverse_complement = ''
         for base in query_string:
-            reverse_complement += BASE_PAIRING[base]
+            try:
+                reverse_complement += BASE_PAIRING[base]
+            except KeyError:
+                 error = True
+                 break
         context['query_string'] = query_string
-        context['reverse_complement'] = reverse_complement
+        if error:
+           highlighted_errors = '<span>\n'
+           for base in query_string:
+             if base not in BASE_PAIRING.keys():
+                 highlighted_errors += '<span style="color: red;font-weight:bold">' + base +'</span>'
+             else:
+                 highlighted_errors += base
+           highlighted_errors += '</span>'
+           context['bad_bases'] = highlighted_errors
+        else: 
+            context['reverse_complement'] = reverse_complement
 
     return render(request, 'home.html', context)
